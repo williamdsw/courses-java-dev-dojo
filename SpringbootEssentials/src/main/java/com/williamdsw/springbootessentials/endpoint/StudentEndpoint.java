83a371610a8e119b5c3c8bab77e6f4eedbccba0c
William Santos <williamdsw@outlook.com>
2020-01-06 16:43:06 -0300

    Spring Boot Essentials 06 - Padroes REST e POST Parte 2

--- SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/endpoint/StudentEndpoint.java
+++ SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/endpoint/StudentEndpoint.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,9 +40,14 @@ public class StudentEndpoint
         {
             return new ResponseEntity<>(new CustomErrorType ("Student not found"), HttpStatus.NOT_FOUND);
         }
-        else
-        {
-            return new ResponseEntity<>(Student.getStudents ().get (index), HttpStatus.OK);
-        }
+        
+        return new ResponseEntity<>(Student.getStudents ().get (index), HttpStatus.OK);
+    }
+    
+    @RequestMapping (method = RequestMethod.POST)
+    public ResponseEntity<?> save (@RequestBody Student student)
+    {
+        Student.getStudents ().add (student);
+        return new ResponseEntity<> (student, HttpStatus.OK);
     }
 }
