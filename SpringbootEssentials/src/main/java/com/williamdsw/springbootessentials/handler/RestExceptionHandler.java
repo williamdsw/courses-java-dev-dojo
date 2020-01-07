5513ad4f74252f1b8bd496f8d2a377739542e276
William Santos <williamdsw@outlook.com>
2020-01-07 14:36:52 -0300

    Spring Boot Essentials 16 - Ordenacao em requisicoes REST

--- SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/handler/RestExceptionHandler.java
+++ SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/handler/RestExceptionHandler.java
@@ -7,10 +7,10 @@ import com.williamdsw.springbootessentials.error.ValidationErrorDetails;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -34,6 +34,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler
         HttpStatus notFound = HttpStatus.NOT_FOUND;
         ResourceNotFoundDetails details = new ResourceNotFoundDetails ("Resource not found", notFound.value (), exception.getMessage (), new Date ().getTime (), exception.getClass ().getName ());
         return new ResponseEntity<>(details, notFound);
+    }
+    
+    @ExceptionHandler (PropertyReferenceException.class)
+    public ResponseEntity<?> handlePropertyReferenceException (PropertyReferenceException exception)
+    {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ErrorDetails details = new ErrorDetails ("Property not found!", badRequest.value (), exception.getMessage (), new Date ().getTime (), exception.getClass ().getName ());
+        return new ResponseEntity<>(details, badRequest);
     }
     
     //------------------------------------------------------------------------//
