e78a5d341b1869122edd0ee5cd278aaa26bd6fb9
William Santos <williamdsw@outlook.com>
2020-01-08 14:30:25 -0300

    Spring Boot Essentials 22 - Cliente Spring com RestTemplate Parte 02 - GET Pageable

--- SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/javaclient/JavaSpringClientTest.java
+++ SpringbootEssentials/src/main/java/com/williamdsw/springbootessentials/javaclient/JavaSpringClientTest.java
@@ -1,5 +1,6 @@
 package com.williamdsw.springbootessentials.javaclient;
 
+import com.williamdsw.springbootessentials.model.PageableResponse;
 import com.williamdsw.springbootessentials.model.Student;
 import java.util.Arrays;
 import java.util.List;
@@ -17,8 +18,10 @@ public class JavaSpringClientTest
 {
     public static void main (String[] args)
     {
-        getStudent ();
-        getStudents ();
+        //getStudent ();
+        //getStudents ();
+        //getPageableStudents ();
+        getPageableStudentsSorted ();
     }
     
     //------------------------------------------------------------------------//
@@ -53,5 +56,33 @@ public class JavaSpringClientTest
         
         System.out.println (Arrays.toString (students));
         System.out.println (entity.getBody ());
+    }
+    
+    private static void getPageableStudents () throws RestClientException
+    {
+        String rootUri = "http://localhost:8080/v1/protected/students";
+        String username = "dave";
+        String password = "devdojo";
+        
+        // Conexao e recupera o objeto
+        RestTemplate restTemplate = new RestTemplateBuilder ().rootUri (rootUri).basicAuthentication (username, password).build ();
+        ParameterizedTypeReference<PageableResponse<Student>> reference = new ParameterizedTypeReference<PageableResponse<Student>> (){};
+        ResponseEntity<PageableResponse<Student>> entity = restTemplate.exchange ("/", HttpMethod.GET, null, reference);
+        List<Student> students = entity.getBody ().getContent ();
+        System.out.println (students);
+    }
+    
+    private static void getPageableStudentsSorted () throws RestClientException
+    {
+        String rootUri = "http://localhost:8080/v1/protected/students";
+        String username = "dave";
+        String password = "devdojo";
+        
+        // Conexao e recupera o objeto
+        RestTemplate restTemplate = new RestTemplateBuilder ().rootUri (rootUri).basicAuthentication (username, password).build ();
+        ParameterizedTypeReference<PageableResponse<Student>> reference = new ParameterizedTypeReference<PageableResponse<Student>> (){};
+        ResponseEntity<PageableResponse<Student>> entity = restTemplate.exchange ("/?sort=name,desc&sort=email,asc", HttpMethod.GET, null, reference);
+        List<Student> students = entity.getBody ().getContent ();
+        System.out.println (students);
     }
 }
